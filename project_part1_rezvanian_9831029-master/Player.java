a686bf2a817a9009c922973e15cb77394f44873f
Haj-Rezvan <58388933+hajrezvan@users.noreply.github.com>
2020-04-10 16:47:20 +0430

    Version 1.2
    Add "getIntPlayerTurn()" method.

--- project_part1_rezvanian_9831029-master/Player.java
+++ project_part1_rezvanian_9831029-master/Player.java
@@ -1,8 +1,30 @@
 /**
  * This class represents status and information of players.
  * @author Rezvanian 9831029
- * @version 0.0
+ * @version 1.2
  */
 public class Player implements IPlayer {
     private int colorCode;              //Color code for white is 1 and for black is 0
+    private boolean playerTurn;
+    private int intPlayerTurn;
+
+    public Player(int color){
+        colorCode = color;
+    }
+
+    public int getColorCode() {
+        return colorCode;
+    }
+
+    public int getIntPlayerTurn() {
+        return intPlayerTurn;
+    }
+
+    public void setPlayerTurn(boolean playerTurn) {
+        this.playerTurn = playerTurn;
+    }
+
+    public void setColorCode(int colorCode) {
+        this.colorCode = colorCode;
+    }
 }
