3d6cb03bbe3adb77a56b47fa46fb28be38083b4c
Haj-Rezvan <58388933+hajrezvan@users.noreply.github.com>
2020-04-10 16:43:33 +0430

    Update GamePlay.java
    Add sleep method and check end of game in other class.

--- project_part1_rezvanian_9831029-master/GamePlay.java
+++ project_part1_rezvanian_9831029-master/GamePlay.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
-
+/**
+ * This class, is main class in program.
+ * The program started from hear.
+ * @version 1.2
+ * @author Rezvanian 9831029
+ * */
 public class GamePlay {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Board mapGame = new Board();
-        mapGame.printMap();
+        Board.initialisingBoard();
 
         Nuts nuts1B = new Nuts('b',4,'D');
         Nuts nuts2B = new Nuts('b',5,'E');
@@ -11,6 +15,18 @@ public class GamePlay {
         Nuts nuts1W = new Nuts('w',4,'E');
         Nuts nuts2W = new Nuts('w',5,'D');
 
-        mapGame.refresh();
+        Player player1 = new Player(1);
+        Player player2 = new Player(0);
+
+        Board.refresh();
+        Thread.sleep(1000);
+
+        //check end of the game
+        do {
+            System.out.println("Welcome players!!");
+            Thread.sleep(1000);
+            System.out.println("Player");
+
+        }while (GameController.checkEndOfGame());
     }
 }
