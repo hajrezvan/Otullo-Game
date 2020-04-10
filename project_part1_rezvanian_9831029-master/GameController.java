b900bdf36115d6071eb6459b1f7524319784279f
Haj-Rezvan <58388933+hajrezvan@users.noreply.github.com>
2020-04-10 16:48:30 +0430

    Create GameController.java
    This class control the end of game and player's turn. 
    it only access in version 1.2 or latest.

---
+++ project_part1_rezvanian_9831029-master/GameController.java
@@ -0,0 +1,9 @@
+public class GameController {
+    /**
+     * this method check end of the game.
+     * */
+    public static boolean checkEndOfGame(){
+        boolean validate = true;
+        return validate;
+    }
+}
