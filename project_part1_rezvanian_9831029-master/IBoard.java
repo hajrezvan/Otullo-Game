75b97e9d0899097feeb585bcab3876715884a97e
Haj-Rezvan <58388933+hajrezvan@users.noreply.github.com>
2020-04-10 16:44:20 +0430

    version 1.2
    Add javadoc and update version to 1.2

--- project_part1_rezvanian_9831029-master/IBoard.java
+++ project_part1_rezvanian_9831029-master/IBoard.java
@@ -1,10 +1,23 @@
 import java.io.IOException;
-
+/**
+ * This interface, has declaration of method in Board class
+ * @version 1.2
+ * @author Rezvanian 9831029
+ * */
 public interface IBoard {
     char[] getMap();
     char[][] getVisualMap();
-    void printMap() throws IOException, InterruptedException;
-    void setMapByPosition(int position, char nut);
-    void setVisualMapByPosition(int xPosition, int yPosition, char nut);
+    static void printMap() throws IOException, InterruptedException {
+
+    }
+
+    //    void setMapByPosition(int position, char nut);
+    //    void setVisualMapByPosition(int xPosition, int yPosition, char nut);
+
     int searchPosition(char c);
+    void setMap(char[] map);
+    void setVisualMap(char[][] visualMap);
+    static void refresh() throws IOException, InterruptedException {
+
+    }
 }
